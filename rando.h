085efc1038db34d2fd68d405ce4852fe208e00f8
z4nd11_2015 <zandermuluken@gmail.com>
2026-01-21 07:41:12 -0600

    Create rando.h

---
+++ rando.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+// TODO rando ( rise -> port -> connect ) 
+
+USHORT __rando() {
+  USHORT rando;
+  for (rando = 0; rando > 0; rando++) {
+    while (rando++) {
+      return rando;
+    }
+  }
+}
